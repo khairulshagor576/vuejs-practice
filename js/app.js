54004db9f9bae92c8b042f7c7fe06e57fc89c949
khairul islam <khairulsagor567@gmail.com>
2021-12-09 16:18:23 +0600

    string interpulation and attribute binging learning

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            msg:"Hi, Mohammad Khaiurl Islam",
+            information:{
+                name:"Mohammad Khaiurl Islam",
+                email:"khairul@outlook.com",
+                githublink:"https://github.com/khairulshagor576"
+            }
+        }
+    }
+});
+
+app.mount("#app");
