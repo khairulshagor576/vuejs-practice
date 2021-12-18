1f7dcd1fb899de77d415ea496246a71b14f497b0
khairul islam <khairulsagor567@gmail.com>
2021-12-18 23:38:39 +0600

    List Rendering using v-for loop

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 const app = Vue.createApp({
     data(){
         return {
-            activeClass:"",
+            skills:[
+            {name:"HTML",exprience:5},
+            {name:"CSS",exprience:6},
+            {name:"Bootstrap",exprience:7},
+            {name:"JS",exprience:10}],
+            newSkill:{},
         }
     },
-    methods: {
-        setClass(colorValue){
-         this.activeClass=colorValue
+    methods:{
+        addNewSkill()
+        {
+            this.skills.push({name:this.newSkill,exprience:0});
+            this.newSkill={};
+        },
+        removeSkill(i)
+        {
+          this.skills.splice(i,1);
         }
-    },
-    
+    }
+
 });
 
 app.mount("#app");
